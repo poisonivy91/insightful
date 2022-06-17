49c9d20fd6f64e710c1ff27685c4f32a748e6cc4
Malik DePasse <92892753+ToastyVal@users.noreply.github.com>
2022-06-17 14:50:43 -0700

    changes

--- src/components/Information.js
+++ src/components/Information.js
@@ -7,15 +7,15 @@ function Info() {
   return (
     <div name="top">
       <Navbar />
-      <div class="container">
+      <div class="info">
         <br />
         <h1>Evidence-Based Information</h1>
         <br />
-        <h3>
+        <h3 id='info'>
           Evidence-based recommendations for natural bodybuilding contest
           preparation: nutrition and supplementation
           <br />
-          <p>
+          <p id='info'>
             The article on evidence-based recommendations has all sorts of
             information including nutrition methods, calorie intake, and
             different types of diets to make your journey to a healthier you
@@ -29,11 +29,11 @@ function Info() {
           </a>
         </h3>
         <br />
-        <h3>
+        <h3 id='info'>
           Effects of diet cycling on weight loss, fat loss and resting energy
           expenditure in women
           <br />
-          <p>
+          <p id='info'>
             Effects of diet cycling is a short but information filled article
             that gives methods, Results, and evidence that short-term diet
             cyclingmay be an effective way to promote weight loss.
@@ -46,10 +46,10 @@ function Info() {
           </a>
         </h3>
         <br />
-        <h3>
+        <h3 id='info'>
           Journal on Targeting Abdominal Obesity Through the Diet
           <br />
-          <p>
+          <p id='info'>
             A detailed joural to talk about abdominal obesity and how to assess,
             diagnose, and get healthier.
           </p>
@@ -61,11 +61,11 @@ function Info() {
           </a>
         </h3>
         <br />
-        <h3>
+        <h3 id='info'>
           Skin Calipers
-          <p>Jackson & Pollock 3-Site Skin-Fold Test (Men)</p>
+          <p id='info'>Jackson & Pollock 3-Site Skin-Fold Test (Men)</p>
           <img id='Skin-Fold' src="https://www.estimatebodyfat.com/static/images/body-fat-caliper-measurement-jackson-pollock-3-site-men.png" />
-          <p>Jackson & Pollock 3-Site Skin-Fold Test (Women)</p>
+          <p id='info'>Jackson & Pollock 3-Site Skin-Fold Test (Women)</p>
           <img id='Skin-Fold' src="https://www.estimatebodyfat.com/static/images/body-fat-caliper-measurement-jackson-pollock-3-site-women.png" />
         </h3>
         <Footer />
