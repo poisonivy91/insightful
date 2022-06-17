356923f3e9e19860ec03d491cdb5c54db28408f7
JonathanMcNeal <93370368+JonathanMcNeal@users.noreply.github.com>
2022-06-17 12:28:34 -0500

    Completed BodyCompCrud.js file

--- src/components/BodyCompCRUD.js
+++ src/components/BodyCompCRUD.js
@@ -1,11 +1,29 @@
 import { async } from "@firebase/util";
 import { useState, useEffect } from "react";
 import {db} from './firebase';
-import {collection, getDocs} from 'firebase/firestore';
+import {collection, getDocs, addDoc, updateDoc, doc, deleteDoc} from 'firebase/firestore';
 
 function BodyCompCRUD(){
+    const [newName, setNewName] = useState("");
+    const [newAge, setNewAge] = useState(0);
     const [users, setUsers] = useState([]);
     const usersCollectionRef = collection(db,users)
+
+    const createBodyComp = async () => {
+        await addDoc(usersCollectionRef, {name: newName, age: Number(newAge)});
+    };
+
+    const updateUser = async(id, age) => {
+        const userDoc = doc(db, "users", id);
+        const newFields = {age: age + 1}
+        await updateDoc(userDoc, newFields)
+    };
+
+    const deleteUser = async (id) => {
+        const userDoc = doc(db, "users", id);
+        await deleteDoc(userDoc)
+    }
+
     useEffect(() => {
        const getUsers = async () =>{
         const data = await getDocs(usersCollectionRef)
@@ -14,7 +32,12 @@ function BodyCompCRUD(){
        }
        getUsers()
     }, [])
-    return <div className = "BodyCompCRUD">{users.map((user)=> {return <div><h3>Name: {user.name}</h3><h3>Age: {user.age}</h3></div>})}</div>;
+    return <div className = "BodyCompCRUD">
+        <input placeholder="Name..." onChange={(event) => {setNewName(event.target.value)}}/>
+        <input type= 'number' placeholder="Age..." onChange={(event) => {setNewAge(event.target.value)}}/>
+        <button onClick={createBodyComp}>Body Composition Results</button>
+        {users.map((user)=> {
+            return <div><h3>Name: {user.name}</h3><h3>Age: {user.age}</h3> <button onClick={()=> {updateUser(user.id, user.age)}}> Increase Age</button><button onClick={() => {deleteUser(user.id)}}></button></div>})}</div>;
 }
 
 export default BodyCompCRUD;
