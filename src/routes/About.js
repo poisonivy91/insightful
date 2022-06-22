23d6ed83f59a6132ee5387e499a9d2acb8dcec50
JonathanMcNeal <93370368+JonathanMcNeal@users.noreply.github.com>
2022-06-22 07:50:03 -0500

    Corrected About.js web app description

--- src/routes/About.js
+++ src/routes/About.js
@@ -11,7 +11,7 @@ const About = () => {
         <HeroImage heading='ABOUT US' text='Provides our users with a simple, accurate and in-depth application for goal focused fitness.
 We do this by helping the user calculate their body composition attributes, which includes BMR, Macros and body fat. Additional to that the user will be given their specific HRmax and the target HR ranges to insure they are getting the most out of their workout sessions.
  Presenting them with fundamental fitness knowledge such as exercise recommendations, repetition, and set ranges will ensure a productive start in accomplishing their fitness goals.
-Insightfully fit also provides our users access to research backed articles about fat loss, muscle building, and sports nutrition. This is achieved by our custom designed API that curates those specific topics from several accredited sports and nutrition publications.
+Insightfully fit also provides our users access to research backed articles about fat loss, muscle building, and sports nutrition from several accredited sports and nutrition publications.
 '/>
         <AboutCards />
         <Footer />
